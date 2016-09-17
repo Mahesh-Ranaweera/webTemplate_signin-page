3135a0d6bc5bec7eb38423328968f9bfc55fdb88
Mahesh Ranaweera <mi6softlab@gmail.com>
2016-09-17 15:30:23 -0400

    code update

---
+++ js/pg-action.js
@@ -0,0 +1,11 @@
+function loginBtn() {
+  document.getElementById('signin-wrapper').style.visibility = 'visible';
+}
+
+function registerBtn() {
+  document.getElementById('register-wrapper').style.visibility = 'visible';
+}
+
+function exitBtn(e) {
+  document.getElementById(e).style.visibility = 'hidden';
+}
